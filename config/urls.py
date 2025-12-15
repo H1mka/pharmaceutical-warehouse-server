5b569efbea29ee8da80a3252d84fdee85d4b98ab
Dmitro Adieliev <adelevdmitriy@gmail.com>
2025-12-16 00:10:58 +0200

    update project structure and add .gitignore

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for pharmaceutical_warehouse project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home)
+    path('admin/', admin.site.urls)
 ]
