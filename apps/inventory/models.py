ff89d673b3ddbb47b6b5572c5f3da398fd926a93
Dmitro Adieliev <adelevdmitriy@gmail.com>
2026-02-18 20:03:50 +0100

    add product model

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from mongoengine import Document, StringField, IntField, DateTimeField
+import datetime
 
 # Create your models here.
+
+class Product(Document):
+    sku = StringField(required=True, unique=True) # unique articule
+    name = StringField(required=True)
+    manufacturer = StringField()
+    form = StringField()
+    dosage = StringField()
+    package_size = IntField()
+    expiration_date = DateTimeField()
+    created_at = DateTimeField(default=datetime.datetime.utcnow)
+    updated_at = DateTimeField(default=datetime.datetime.utcnow)
+
+    meta = {'collection': 'products'}
